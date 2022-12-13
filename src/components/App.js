fca72d86b7deb8d626d9e3d35075501899e477d2
Akash Maurya <17akashma@gmail.com>
2022-12-13 19:28:11 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((x) => (
+        <Note Key={x.id} title={x.title} content={x.content} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
